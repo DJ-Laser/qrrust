31074d8b84e9f72c2b76fcd0fdf38a89aecaad42
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-01-23 01:03:54 -0500

    clean up code a bit

--- src/io.rs
+++ src/io.rs
@@ -6,34 +6,6 @@ pub use write::*;
 pub const STDOUT_FILENO: u32 = 1;
 pub const STDERR_FILENO: u32 = 1;
 
-#[macro_export]
-macro_rules! print {
-    ($($arg:expr),+) => {
-      write!($crate::io::STDOUT_FILENO, $($arg),+)
-    };
-}
-
-#[macro_export]
-macro_rules! println {
-    ($($arg:expr),+) => {
-      writeln!($crate::io::STDOUT_FILENO, $($arg),+);
-    };
-}
-
-#[macro_export]
-macro_rules! eprint {
-    ($($arg:expr),+) => {
-      write!($crate::io::STDERR_FILENO, $($arg),+)
-    };
-}
-
-#[macro_export]
-macro_rules! eprintln {
-    ($($arg:expr),+) => {
-      writeln!($crate::io::STDERR_FILENO, $($arg),+)
-    };
-}
-
 /// Gets the length of a c-string as bytes (unsafe for onvious reasons *ahem* null terminatiors)
 pub unsafe fn strlen(mut s: *const u8) -> usize {
   unsafe {
@@ -49,3 +21,29 @@ pub unsafe fn strlen(mut s: *const u8) -> usize {
 pub unsafe fn to_cstr_slice(s: *const u8) -> &'static [u8] {
   unsafe { mkslice(s, strlen(s)) }
 }
+
+macro_rules! print {
+  ($($arg:expr),+) => {
+    $crate::io::write!($crate::io::STDOUT_FILENO, $($arg),+)
+  };
+}
+
+macro_rules! println {
+  ($($arg:expr),+) => {
+    $crate::io::writeln!($crate::io::STDOUT_FILENO, $($arg),+);
+  };
+}
+
+macro_rules! eprint {
+  ($($arg:expr),+) => {
+    $crate::io::write!($crate::io::STDERR_FILENO, $($arg),+)
+  };
+}
+
+macro_rules! eprintln {
+  ($($arg:expr),+) => {
+    $crate::io::writeln!($crate::io::STDERR_FILENO, $($arg),+)
+  };
+}
+
+pub(crate) use {eprint, eprintln, print, println};
